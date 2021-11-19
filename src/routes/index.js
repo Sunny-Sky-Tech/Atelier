8e8b6aa486ce3edfeb7368b6d5dec6d0e961d2a3
Khai Tran <kqtran917@yahoo.com>
2021-11-19 12:16:40 -0600

    Implemented finished skeleton from Kent C Dodds examples

---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+// any other routes imports would go here
+// example
+// import {getMathRoutes} from './math'
+
+function getRoutes() {
+  // create a router for all the routes of our app
+  const router = express.Router()
+
+  // example
+  // router.use('/math', getMathRoutes())
+  // any additional routes would go here
+
+  return router
+}
+
+export {getRoutes}
