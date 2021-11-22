1d31f99e5a8df5909371b9e2cd672fbed4774f41
Khai Tran <kqtran917@yahoo.com>
2021-11-22 17:19:23 -0600

    Added k6 to packages

--- index.js
+++ index.js
@@ -6,8 +6,21 @@ const cors = require('cors')
 const controller = require('./server/controller/index.js')
 const bodyParser = require("body-parser");
 
-app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, 'Content-Type' : 'multipart/form-data' ,* "
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
 
+app.use(bodyParser.json());
+app.use(cors())
 
 
 module.exports.app = app;
