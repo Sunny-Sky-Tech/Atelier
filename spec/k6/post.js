248e5f061e1f28a4a0e6ff7ee66329b920b2e245
Khai Tran <kqtran917@yahoo.com>
2021-11-24 20:55:26 -0600

    Implemented stage testing for post requests

--- spec/k6/post.js
+++ spec/k6/post.js
@@ -2,12 +2,12 @@ import http from 'k6/http';
 
 export default function () {
   const url = 'http://localhost:8080/qa/questions';
-  const payload = JSON.stringify([{
+  const payload = JSON.stringify({
     body: "Bob",
     name: "jack",
     email: 'aaa@aaa.com',
     product_id: 1
-  }]);
+  });
 
   const params = {
     headers: {
